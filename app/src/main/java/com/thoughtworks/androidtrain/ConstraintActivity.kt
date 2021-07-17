b5e4f6881592972410f0174dc809e123868458c5
mutoe <mutoe@foxmail.com>
2021-07-17 12:30:25 +0800

    feat: implement constraint layout

---
+++ app/src/main/java/com/thoughtworks/androidtrain/ConstraintActivity.kt
@@ -0,0 +1,11 @@
+package com.thoughtworks.androidtrain
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout)
+    }
+}
--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.thoughtworks.androidtrain
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -14,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "MainActivity Created")
+    }
+
+    fun openConstraintActivity(view: View) {
+        val intent = Intent(this, ConstraintActivity::class.java)
+        startActivity(intent)
     }
 }
