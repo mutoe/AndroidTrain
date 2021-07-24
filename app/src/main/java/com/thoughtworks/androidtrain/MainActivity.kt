3e32e076ff5012a60936c246795718e71e5fdf9a
mutoe <mutoe@foxmail.com>
2021-07-24 16:54:58 +0800

    feat: implement pick contact

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.thoughtworks.androidtrain
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+const val REQUEST_SELECT_PHONE_NUMBER = 1
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -26,5 +31,41 @@ class MainActivity : AppCompatActivity() {
     fun openLoginActivity(view: View) {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+    }
+
+    fun openContact(view: View) {
+        val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+            .addCategory(Intent.CATEGORY_DEFAULT)
+            .setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE)
+
+        startActivityForResult(intent, REQUEST_SELECT_PHONE_NUMBER)
+        Log.d(TAG, "active contact intent")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.d(TAG, "onActivityResult")
+
+        if (requestCode == REQUEST_SELECT_PHONE_NUMBER && resultCode == RESULT_OK && data?.data != null) {
+            val contactUri = data.data!!
+            val projection = arrayOf(
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+            )
+            val cursor = contentResolver.query(contactUri, projection, null, null, null)
+            Log.d(TAG, cursor?.count.toString())
+
+            if (cursor?.moveToFirst() == true) {
+                val contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                val contactNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                cursor.close()
+
+                Log.d(TAG, "$contactName $contactNumber")
+                Toast.makeText(this, "$contactName $contactNumber", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
     }
 }
