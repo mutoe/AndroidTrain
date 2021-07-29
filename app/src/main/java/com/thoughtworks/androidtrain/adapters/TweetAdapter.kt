5e8548e199a84ba97e6dee51ef4fc964711342f5
mutoe <mutoe@foxmail.com>
2021-07-29 15:00:23 +0800

    feat: implement view type in recycler view

--- app/src/main/java/com/thoughtworks/androidtrain/adapters/TweetAdapter.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/adapters/TweetAdapter.kt
@@ -11,32 +11,50 @@ import com.thoughtworks.androidtrain.model.Tweet
 class TweetAdapter(private var dataset: List<Tweet>) :
     RecyclerView.Adapter<TweetAdapter.ViewHolder>() {
 
-    fun setTweets(tweets: List<Tweet>) {
-        dataset = tweets
-        notifyDataSetChanged()
+    companion object {
+        const val FOOTER_VIEW = 1
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nickNameView: TextView
-        val contentView: TextView
+    open class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-        init {
-            nickNameView = view.findViewById(R.id.tweet_nickname)
-            contentView = view.findViewById(R.id.tweet_content)
+    class NormalViewHolder(view: View) : ViewHolder(view) {
+        val nickNameView: TextView = view.findViewById(R.id.tweet_nickname)
+        val contentView: TextView = view.findViewById(R.id.tweet_content)
+    }
+
+    class FooterViewHolder(view: View) : ViewHolder(view)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+            FOOTER_VIEW -> {
+                val view = LayoutInflater.from(viewGroup.context)
+                    .inflate(R.layout.recycler_view_footer, viewGroup, false)
+                FooterViewHolder(view)
+            }
+            else -> {
+                val view = LayoutInflater.from(viewGroup.context)
+                    .inflate(R.layout.tweet_row_item, viewGroup, false)
+                NormalViewHolder(view)
+            }
         }
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.tweet_row_item, viewGroup, false)
-
-        return ViewHolder(view)
-    }
-
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.nickNameView.text = dataset[position].sender.nick
-        viewHolder.contentView.text = dataset[position].content
+        when (viewHolder) {
+            is NormalViewHolder -> {
+                viewHolder.nickNameView.text = dataset[position].sender.nick
+                viewHolder.contentView.text = dataset[position].content
+            }
+        }
     }
 
-    override fun getItemCount(): Int = dataset.size
+    override fun getItemCount(): Int = dataset.size + 1
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == dataset.size) {
+            return FOOTER_VIEW
+        }
+        return super.getItemViewType(position)
+    }
+
 }
